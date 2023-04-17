6217dd49ed5c4cac605a177465e8099bd78ec3ed
yoshiya0448 <yoshibasket86@gmail.com>
2023-04-17 18:30:42 +0900

    ✨ imageをバックエンドのホストから取得できるように設定

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "8000",
+        // pathname: "/account123/**",
+      },
+    ],
+  },
+};
