3a7c2502f335d19317503d2435f1cf5ea0ce1a15
yoshiya0448 <yoshibasket86@gmail.com>
2023-04-17 18:36:15 +0900

    ⚡️ 共通コンポーネントの配置

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "~/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "~/components/layout/layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1 " />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
